6170bc623b3a0d1bce4c9296753fa6ee10f43122
Keshav Gautam <keshavgautam375@gmail.com>
2024-01-05 15:39:16 +0530

    added utils

---
+++ Backend/src/utils/asyncHandler.js
@@ -0,0 +1,35 @@
+const asyncHAndler =(requestHandler) => {
+(req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next))
+    .catch((err) => next(err)) // if we have an error in the requestHandler function then we will pass the error to the next middleware function (error handler middleware function
+    }
+
+}
+
+
+
+
+
+export default asyncHAndler
+
+
+
+
+
+
+// const asyncHandler = () => {}
+// const asyncHandler = (func) =>{ () => {}}
+// const asyncHandler = (func) => () => {}
+// const asyncHandler = (func) => async () => {}
+
+// const asyncHandler = (fn) => async (req, res, next) => { //higher order function functions that takes a function as an argument and returns a function
+
+//     try{
+//         await fn(req, res, next)
+//     } catch(err){
+//         res.status(err.code || 500).json({
+//             success:false,
+//             message: err.message || "Internal Server Error"
+//         })
+//     }
+// }
