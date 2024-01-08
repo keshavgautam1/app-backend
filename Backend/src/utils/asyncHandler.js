dcae238d12a320aa8d700eacabef1a27c9054329
Keshav Gautam <keshavgautam375@gmail.com>
2024-01-08 13:25:39 +0530

    all  setup configured

--- Backend/src/utils/asyncHandler.js
+++ Backend/src/utils/asyncHandler.js
@@ -1,16 +1,13 @@
-const asyncHAndler =(requestHandler) => {
-(req, res, next) => {
+const asyncHandler =(requestHandler) => {
+return (req, res, next) => {
     Promise.resolve(requestHandler(req, res, next))
     .catch((err) => next(err)) // if we have an error in the requestHandler function then we will pass the error to the next middleware function (error handler middleware function
     }
-
 }
 
 
 
-
-
-export default asyncHAndler
+export {asyncHandler}
 
 
 
